677b8a47b74246a8df5cad4fb6f8834fafa11c04
aldomq <58537416+aldomq@users.noreply.github.com>
2024-11-09 14:20:48 -0600

    Start

---
+++ src/main/java/com/mycompany/caso_estudio_1/Caso_Estudio_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caso_estudio_1;
+
+/**
+ *
+ * @author aldoi
+ */
+public class Caso_Estudio_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
